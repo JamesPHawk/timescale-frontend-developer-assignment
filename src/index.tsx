3bb79c43924bcecb0d1122deb9e339c955cec012
JamesPHawk <pjhawk208@gmail.com>
2024-11-03 13:26:55 -0600

    Adding chakra ui to project

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,14 @@ import * as React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
+import { Provider } from "./components/ui/provider";
 
 const domNode = document.getElementById("root");
 const root = createRoot(domNode);
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
